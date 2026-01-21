5c3477c302e266dbf425640c6d4e31a798300124
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-21 12:30:24 +0530

    Setup the backend

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+
+
+const app=express();
+
+const PORT=process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({Credentials:true}));
+
+
+app.get('/', (req, res) => {
+    res.send("started building backend");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
